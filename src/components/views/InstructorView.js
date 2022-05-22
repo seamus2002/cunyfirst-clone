f0e6de738524c2b56933babbe5230fa20ddd7c2d
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-05-22 12:14:50 -0400

    update instructor view

--- src/components/views/InstructorView.js
+++ src/components/views/InstructorView.js
@@ -1,18 +1,42 @@
+import { Link } from "react-router-dom";
+
+
 const InstructorView = (props) => {
-  const {instructor} = props;
+  const {instructor, editCourse, allCourses} = props;
+  let assignedCourses = allCourses.filter(course => course.instructorId===instructor.id);
+  let availableCourses = allCourses.filter(course => course.instructorId!==instructor.id);
+  
   return (
     <div className="container">      
       <h1 className="page-header">{instructor.firstname} {instructor.lastname}</h1>
-      <br />
-      <h3>Department</h3>
-      <p>{instructor.department}</p>
-      <br />
-      <h3>Courses Taught</h3>
-      {instructor.courses.map( course => {
-        return (
-          <p key={course.id}>{course.title}</p>
-        );
-      })}
+      <h3 className="page-header">Department: {instructor.department}</h3>
+      <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
+        <div>Assigned courses:
+        {assignedCourses.map( course => {
+          return (
+            <div key={course.id}>
+            <Link to={`/course/${course.id}`}>
+              <h4>{course.title}</h4>
+            </Link>
+            <button onClick={() => editCourse({id:course.id, instructorId: null})}>x</button>
+            </div>
+          );
+        })}</div>
+        <div>Available courses:
+        {availableCourses.map( course => {
+          return (
+            <div key={course.id}>
+            <Link to={`/course/${course.id}`}>
+              <h4>{course.title}</h4>
+            </Link>
+            <button onClick={() => editCourse({id:course.id, instructorId: instructor.id})}>+</button>
+            </div>
+          );
+        })}</div>
+
+      </div>
+
+  
     </div>
   );
 
