9e0d22255d212b085cf98239025dabfb637a748a
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-05-23 13:19:52 -0400

    add timeslot and course to course view

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -4,7 +4,11 @@ const CourseView = (props) => {
     <div className="container">
       <h1 className="page-header">{course.title}</h1>
       <h3>Instructor</h3>
-      {course.instructor ? <p>{course.instructor.firstname + " " + course.instructor.lastname}</p>: <p>staff</p>}
+      {course.instructor ? <p>{course.instructor.firstname + " " + course.instructor.lastname}</p> : <p>staff</p>}
+      <h3>Timeslot</h3>
+      {course.timeslot ? <p>{course.timeslot}</p> : <p>Asynchronous</p>}
+      <h3>Location</h3>
+      {course.location ? <p>{course.location}</p> : <p>Online</p>}
     </div>
   );
 
