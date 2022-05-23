d6982ab2da72a103791a05af9124d9a1c31de531
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-05-23 12:22:30 -0400

    add instructors reducers

--- src/store/reducers/instructors.js
+++ src/store/reducers/instructors.js
@@ -5,6 +5,16 @@ const allInstructors = (state = [], action) => {
   switch (action.type) {
     case at.FETCH_ALL_INSTRUCTORS:
       return action.payload;
+    case at.ADD_INSTRUCTOR:
+      return [...state, action.payload]
+    case at.DELETE_INSTRUCTOR:
+      return state.filter(instructors => instructors.id!==action.payload);
+    case at.EDIT_INSTRUCTOR:
+      return state.map(instructors => { 
+        return (
+          instructors.id===action.payload.id ? action.payload : instructors
+        );
+      });
     default:
       return state;
   }
